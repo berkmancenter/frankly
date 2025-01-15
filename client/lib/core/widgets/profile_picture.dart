b61af7e7f82913761d56177afb4c1d881afba4f8
katherineqian <21981137+katherineqian@users.noreply.github.com>
2025-01-15 13:47:25 -0600

    Use nullish operator to call box shadow if not passed in directly. Rename the parameter to boxShadow from shadow. Define the default borderRadius as a double.

--- client/lib/core/widgets/profile_picture.dart
+++ client/lib/core/widgets/profile_picture.dart
@@ -5,15 +5,11 @@ import 'package:client/styles/app_styles.dart';
 class ProfilePicture extends StatelessWidget {
   const ProfilePicture({
     required this.imageUrl,
-    this.shadow = const BoxShadow(
-      color: Color(0x40000000),
-      blurRadius: 5,
-      offset: Offset(0, 2),
-    ),
-    this.borderRadius = 8,
+    this.boxShadow,
+    this.borderRadius = 8.0,
   });
 
-  final BoxShadow shadow;
+  final BoxShadow? boxShadow;
   final double borderRadius;
   final String imageUrl;
 
@@ -23,7 +19,14 @@ class ProfilePicture extends StatelessWidget {
       decoration: BoxDecoration(
         color: AppColor.white,
         borderRadius: BorderRadius.circular(borderRadius),
-        boxShadow: [shadow],
+        boxShadow: [
+          boxShadow ??
+              BoxShadow(
+                color: Color(0x40000000),
+                blurRadius: 5,
+                offset: Offset(0, 2),
+              ),
+        ],
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(borderRadius),
