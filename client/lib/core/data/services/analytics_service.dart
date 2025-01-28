87f761bc39462dfef0f622fdd6a5ce8fba08b8c5
Jennifer Hickey <jhickey@cyber.harvard.edu>
2025-01-28 09:50:48 -0500

    Enable Plausible analytics for existing analytic events

--- client/lib/core/data/services/analytics_service.dart
+++ client/lib/core/data/services/analytics_service.dart
@@ -1,4 +1,6 @@
 import 'package:data_models/analytics/analytics_entities.dart';
+import 'package:client/services.dart';
+import 'dart:js_interop';
 
 /// Trait names in Segment; avoid modifying existing names as it will break data continuity
 enum UserAnalyticsProperty {
@@ -6,11 +8,30 @@ enum UserAnalyticsProperty {
   displayName,
 }
 
+@JS('plausible')
+external void _plausible(String eventName, [JSAny? eventData]);
+
 /// Manages our Segment integration
 class AnalyticsService {
   /// Call to track a user event
   void logEvent(AnalyticsEvent event) {
-    // Disabling segment by not sending events here rather than removing the log events
-    // everywhere that we log them.
+    _trackEvent(
+      event.getEventType(),
+      props: event.toJson(),
+    );
+  }
+
+  void _trackEvent(String eventName, {Map<String, dynamic>? props}) {
+    try {
+      if (props != null) {
+        loggingService.log('Analytics Event $eventName with some props $props');
+        _plausible(eventName, props.jsify());
+      } else {
+        loggingService.log('Analytics Event $eventName');
+        _plausible(eventName);
+      }
+    } catch (e) {
+      loggingService.log('Failed to track event: $e');
+    }
   }
 }
