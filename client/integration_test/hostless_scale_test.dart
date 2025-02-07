ec63fb053d5abf4dc9dbc9ea10bdb09ec4c0acc9
katherineqian <21981137+katherineqian@users.noreply.github.com>
2025-02-07 12:09:34 -0800

    Update hostless scale test actions.

--- client/integration_test/hostless_scale_test.dart
+++ client/integration_test/hostless_scale_test.dart
@@ -1,5 +1,8 @@
+import 'package:client/features/events/features/live_meeting/presentation/widgets/live_meeting_desktop.dart';
+import 'package:client/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:client/features/events/features/live_meeting/features/video/presentation/widgets/control_bar.dart';
 import 'package:client/features/events/features/event_page/presentation/widgets/event_info.dart';
 import 'package:client/core/widgets/confirm_dialog.dart';
 import 'package:client/app.dart';
@@ -14,6 +17,12 @@ void main() {
   testWidgets('Run event scale test', (WidgetTester tester) async {
     useBotControls = true;
 
+    final testerId = uuid.v4();
+
+    final name = 'Test ${testerId.substring(0, 4)}';
+    final email = '$testerId@mycommunity.test';
+    const password = 'tester123';
+    loggingService.log('Beginning event scale test - user $testerId.');
     await runClient();
 
     await tester.pump();
@@ -22,12 +31,6 @@ void main() {
 
     final scaleTestInfo = ScaleTestService().getScaleTestInfo();
 
-    final testerId = uuid.v4();
-
-    final name = 'Test ${testerId.substring(0, 4)}';
-    final email = '$testerId@mycommunity.test';
-    const password = 'tester123';
-
     await signUpForApp(
       tester,
       name: name,
@@ -35,7 +38,7 @@ void main() {
       password: password,
     );
 
-    await wait(tester, timeout: Duration(seconds: 10));
+    await wait(tester, timeout: Duration(seconds: 3));
 
     routerDelegate.beamTo(
       CommunityPageRoutes(communityDisplayId: scaleTestInfo.communityId)
@@ -45,7 +48,7 @@ void main() {
       ),
     );
 
-    await wait(tester, timeout: Duration(seconds: 10));
+    await wait(tester, timeout: Duration(seconds: 3));
 
     await waitAndTap(tester, find.byKey(EventInfo.rsvpButtonKey));
 
@@ -54,18 +57,21 @@ void main() {
       find.byKey(EventInfo.enterEventButtonKey),
     );
 
-    await waitAndTap(
-      tester,
-      find.byKey(ControlBar.videoToggleButtonKey),
-      timeout: Duration(minutes: 20),
-    );
+    await wait(tester, timeout: Duration(seconds: 5));
 
+    print('Event not started yet');
     await waitAndTap(
       tester,
       find.byKey(ConfirmDialog.confirmButtonKey),
-      timeout: Duration(minutes: 20),
     );
 
-    await wait(tester, timeout: Duration(minutes: 40));
+    // Turn on video.
+    await waitAndTap(
+      tester,
+      find.byKey(ControlBar.videoToggleButtonKey),
+      timeout: Duration(minutes: 5),
+    );
+
+    await wait(tester, timeout: Duration(minutes: 60));
   });
 }
