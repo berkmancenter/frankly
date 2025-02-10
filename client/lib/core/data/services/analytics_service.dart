9056b4f60000fb476ef97c948fcf90dda6757e3c
Jennifer Hickey <jhickey@cyber.harvard.edu>
2025-02-10 15:44:26 -0500

    Enable Matomo analytics
    * Track analytics when Matomo env variables set
    * Modify GitHub action to set env variables when building the app
    * Ensure tracking is cookieless
    * Ensure tracking is persistent such that batched events are sent to Matomo on next client visit if app closed before send

--- client/lib/core/data/services/analytics_service.dart
+++ client/lib/core/data/services/analytics_service.dart
@@ -1,16 +1,43 @@
+import 'package:client/config/environment.dart';
+import 'package:client/services.dart';
 import 'package:data_models/analytics/analytics_entities.dart';
+import 'package:matomo_tracker/matomo_tracker.dart';
 
-/// Trait names in Segment; avoid modifying existing names as it will break data continuity
-enum UserAnalyticsProperty {
-  email,
-  displayName,
-}
-
-/// Manages our Segment integration
 class AnalyticsService {
+  bool enableMatomo = Environment.matomoURL != '';
+
+  Future<void> initialize() async {
+    if (enableMatomo && !MatomoTracker.instance.initialized) {
+      await MatomoTracker.instance.initialize(
+        siteId: Environment.matomoSiteId,
+        url: Environment.matomoURL,
+        cookieless: true,
+        dispatchSettings: const DispatchSettings.persistent(),
+      );
+    }
+  }
+
   /// Call to track a user event
   void logEvent(AnalyticsEvent event) {
-    // Disabling segment by not sending events here rather than removing the log events
-    // everywhere that we log them.
+    if (!enableMatomo) return;
+
+    final eventProps = event.toJson();
+    final communityId = eventProps['communityId'];
+
+    final dimensions = {
+      if (communityId != null) 'dimension1': communityId.toString(),
+      if (userService.currentUserId != null)
+        'dimension3': userService.currentUserId!,
+    };
+
+    MatomoTracker.instance.trackEvent(
+      eventInfo: EventInfo(
+        category: event.getEventCategory(),
+        action: event.getEventType(),
+        name: event.getEventName(),
+        value: event.getMetricValue(),
+      ),
+      dimensions: dimensions,
+    );
   }
 }
