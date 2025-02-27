96c3e3b5135945f038900cd15e6d73ed1471a55f
katherineqian <21981137+katherineqian@users.noreply.github.com>
2025-02-27 12:53:04 -0800

    Migrate TextTheme and IconTheme to theme.dart.

--- client/lib/styles/theme.dart
+++ client/lib/styles/theme.dart
@@ -25,5 +25,41 @@ final appTheme = ThemeData(
     surfaceDim: AppColor.gray400,
     onSurface: AppColor.black,
   ),
+  textTheme: textTheme,
   pageTransitionsTheme: NoTransitionsOnWeb(),
+);
+
+final textTheme = GoogleFonts.poppinsTextTheme(
+  TextTheme(
+    /// displayColor is used for all display styles
+    displayLarge: null,
+    displayMedium: null,
+    displaySmall: null,
+
+    /// headlineLarge: displayColor
+    headlineLarge: AppTextStyle.headline1,
+
+    /// headlineMedium: displayColor
+    headlineMedium: AppTextStyle.headline2,
+    headlineSmall: AppTextStyle.headline3,
+    titleLarge: AppTextStyle.headline4,
+    titleMedium: AppTextStyle.headlineSmall,
+    titleSmall: AppTextStyle.subhead,
+    bodyLarge: AppTextStyle.eyebrow,
+
+    /// bodyMedium is the default text style for Material.
+    /// https://api.flutter.dev/flutter/material/TextTheme/bodyMedium.html
+    bodyMedium: AppTextStyle.body,
+
+    /// bodySmall: displayColor
+    bodySmall: AppTextStyle.bodySmall,
+    labelMedium: AppTextStyle.bodySmall,
+  ).apply(
+    displayColor: AppColor.black,
+    bodyColor: AppColor.black,
+  ),
+);
+
+final iconTheme = IconThemeData(
+  color: AppColor.black,
 );
