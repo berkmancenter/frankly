0b010fbec4213d7ed5ee3c2f1ff6c40d0cc670be
Audrey Tang <audreyt@audreyt.org>
2025-04-20 07:13:19 +0800

    * fix test by registering singleton

--- client/lib/core/localization/localization_helper.dart
+++ client/lib/core/localization/localization_helper.dart
@@ -13,34 +13,3 @@ extension LocalizationExtension on BuildContext {
     return localizations;
   }
 }
-
-/// This is a helper class that shows how to use localized strings in widgets
-/// throughout the application.
-class LocalizationHelper {
-  /// Get the AppLocalizations instance from the current BuildContext
-  /// Also sets the localization in appLocalizationService
-  static AppLocalizations of(BuildContext context) {
-    final localizations = AppLocalizations.of(context)!;
-    // Set the localization in the app localization service
-    appLocalizationService.setLocalization(localizations);
-    return localizations;
-  }
-
-  /// Example of how to use localized strings in a StatelessWidget
-  static Widget exampleWidget(BuildContext context) {
-    final l10n = AppLocalizations.of(context)!;
-    // Set the localization in the app localization service
-    appLocalizationService.setLocalization(l10n);
-
-    return Column(
-      children: [
-        Text(l10n.appTitle),
-        Text(l10n.appDescription),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text(l10n.login),
-        ),
-      ],
-    );
-  }
-}
