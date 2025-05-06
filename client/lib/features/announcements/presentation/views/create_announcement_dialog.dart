463d487a35368d78be4b7a41e90f52dac6654c81
Drago <107933615+leechiuhui@users.noreply.github.com>
2025-05-06 21:24:44 +0800

    Revert "work on #10 translate “Create New Announcement ” in  create_announcement_dialog.dart"
    This reverts commit d3fc5466607f4c8b491658b496ee181f702d3fc1.

--- client/lib/features/announcements/presentation/views/create_announcement_dialog.dart
+++ client/lib/features/announcements/presentation/views/create_announcement_dialog.dart
@@ -54,8 +54,7 @@ class _CreateAnnouncementDialogState extends State<CreateAnnouncementDialog> {
           children: [
             SizedBox(height: 50),
             HeightConstrainedText(
-              // 'Create New Announcement',
-              context.l10n.createNewAnnouncement,
+              'Create New Announcement',
               style: TextStyle(
                 fontSize: 26,
                 fontWeight: FontWeight.bold,
