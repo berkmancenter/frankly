c92e0a7040bb67b8845ab244c52ea0e3c572892c
katherineqian <21981137+katherineqian@users.noreply.github.com>
2025-05-12 16:24:19 -0400

    Fix colors of edit community button.

--- client/lib/features/community/presentation/widgets/edit_community_button.dart
+++ client/lib/features/community/presentation/widgets/edit_community_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:client/features/community/features/create_community/presentation/views/create_community_dialog.dart';
 import 'package:client/features/community/data/providers/community_provider.dart';
-import 'package:client/core/widgets/custom_ink_well.dart';
+
 import 'package:client/services.dart';
 import 'package:client/styles/styles.dart';
 import 'package:client/core/widgets/stream_utils.dart';
@@ -36,24 +36,16 @@ class _EditCommunityButtonState extends State<EditCommunityButton> {
           )
           .asStream(),
       builder: (context, caps) {
-        return CustomInkWell(
-          onTap: () =>
-              _editTapped(canSetDisplayId: caps?.hasCustomUrls ?? false),
-          borderRadius: BorderRadius.circular(15),
-          child: Container(
-            alignment: Alignment.center,
-            width: 30,
-            height: 30,
-            decoration: BoxDecoration(
-              color: context.theme.colorScheme.surfaceContainerLowest,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Icon(
-              Icons.edit,
-              color: context.theme.colorScheme.secondary,
-              size: 20,
-            ),
+        return IconButton.filled(
+          icon: Icon(
+            Icons.edit,
+            color: context.theme.colorScheme.onSecondary,
           ),
+          visualDensity: VisualDensity.compact,
+          iconSize: 24,
+          color: context.theme.colorScheme.secondary,
+          onPressed: () =>
+              _editTapped(canSetDisplayId: caps?.hasCustomUrls ?? false),
         );
       },
     );
