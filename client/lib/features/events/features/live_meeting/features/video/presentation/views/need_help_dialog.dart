b46e9a19a518f97e63e51bb6c1623ef5ef0ca005
Bestian Tang <bestian@gmail.com>
2025-05-27 15:57:00 +0800

    close #23 成功翻譯長篇的needHelpMarkdown

--- client/lib/features/events/features/live_meeting/features/video/presentation/views/need_help_dialog.dart
+++ client/lib/features/events/features/live_meeting/features/video/presentation/views/need_help_dialog.dart
@@ -6,28 +6,13 @@ import 'package:client/core/widgets/buttons/action_button.dart';
 import 'package:client/config/environment.dart';
 import 'package:client/styles/app_styles.dart';
 import 'package:client/core/data/providers/dialog_provider.dart';
+import 'package:client/services.dart';
+import 'package:client/core/localization/localization_helper.dart';
+
 
 const _kTroubleshootingGuideUrl = Environment.troubleshootingGuideUrl;
 
-const needHelpMarkdown = '''
-### Technical Issues?
-
-__Refresh the page:__ Click the refresh button in the upper-right corner or refresh your browser. You will stay in the same room.
-
-__Close other programs:__ Close Zoom, Microsoft Teams, and other programs that use your camera and microphone.
-
-__Check your Audio/Video Settings:__
-- Click the three dots (. . .) in the bottom left and click “Audio/Video Settings” to select the correct camera and microphone.
-- Be sure to “Allow” camera or mic access if prompted.
-
-__Try a different device or browser:__ We recommend Chrome or Firefox on a laptop or desktop, and Safari if using an iPhone or iPad. When you enter the event again you will be placed in the same room.
-''';
-
-const needHelpMarkdownWithContactAdmin = '''
-$needHelpMarkdown
-
-If you still need help, [click here](ask-admin) to request an admin to join your room and assist you as soon as they can.
-''';
+final l10n = appLocalizationService.getLocalization();
 
 class NeedHelpDialog extends StatelessWidget {
   final bool showContactAdmin;
@@ -39,6 +24,8 @@ class NeedHelpDialog extends StatelessWidget {
   }
 
   Widget _buildDialog(BuildContext context) {
+
+    final l10n = appLocalizationService.getLocalization();
     return Material(
       color: Colors.transparent,
       child: GestureDetector(
@@ -76,8 +63,8 @@ class NeedHelpDialog extends StatelessWidget {
               Flexible(
                 child: Markdown(
                   data: showContactAdmin
-                      ? needHelpMarkdownWithContactAdmin
-                      : needHelpMarkdown,
+                      ? l10n.needHelpMarkdownWithContactAdmin
+                      : l10n.needHelpMarkdown,
                   shrinkWrap: true,
                   styleSheet:
                       MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
@@ -97,14 +84,14 @@ class NeedHelpDialog extends StatelessWidget {
               SizedBox(height: 12),
               RichText(
                 text: TextSpan(
-                  text: 'Need more help? ',
+                  text: l10n.needMoreHelp,
                   style: AppTextStyle.bodyMedium.copyWith(
                     color: AppColor.gray2,
                     fontWeight: FontWeight.w500,
                   ),
                   children: [
                     TextSpan(
-                      text: 'Troubleshooting Guide',
+                      text: l10n.troubleshootingGuide,
                       style: AppTextStyle.bodyMedium.copyWith(
                         fontWeight: FontWeight.w500,
                         color: AppColor.accentBlue,
