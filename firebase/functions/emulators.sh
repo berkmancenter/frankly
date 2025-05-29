cb41e03f0842d45265aefdefb4cdc7d3b704e10c
Johnny Richardson <johnny.c.rich@gmail.com>
2025-05-29 17:02:13 -0400

    chore: emulators.sh update.

--- firebase/functions/emulators.sh
+++ firebase/functions/emulators.sh
@@ -1,2 +1,2 @@
 dart run build_runner build --output=build 
-firebase emulators:start --only firestore,functions,auth,pubsub,database
+firebase emulators:start --only firestore,functions,auth,pubsub,database --project=dev
