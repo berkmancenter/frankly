e7f26ce7e3c4512d5554eeb71fc97e90403c1de6
katherineqian <21981137+katherineqian@users.noreply.github.com>
2025-06-03 15:54:51 -0500

    Remove contentPadding where missed.

--- client/lib/features/user/presentation/views/profile_tab.dart
+++ client/lib/features/user/presentation/views/profile_tab.dart
@@ -195,7 +195,7 @@ class _ProfileTabState extends State<_ProfileTab> {
                 textStyle: TextStyle(
                   color: context.theme.colorScheme.primary,
                   fontSize: 16,
-                ),                
+                ),
                 onChanged: (value) {
                   controller.onChangedName(value);
                   context.read<AppDrawerProvider>().setUnsavedChanges(true);
@@ -329,8 +329,6 @@ class _ProfileTabState extends State<_ProfileTab> {
           minLines: 6,
           borderRadius: 5,
           padding: EdgeInsets.zero,
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 10, vertical: 16),
           textStyle:
               TextStyle(color: context.theme.colorScheme.primary, fontSize: 16),
           initialValue: changeRecord.about,
@@ -590,8 +588,6 @@ class SocialInputField extends StatelessWidget {
               color: context.theme.colorScheme.primary,
               fontSize: 16,
             ),
-            contentPadding:
-                const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
             onChanged: onChanged,
           ),
         ),
