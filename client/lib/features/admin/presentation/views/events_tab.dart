09d54b3cc9a50b022f7cde8f60e9e2c8456c314f
Johnny Richardson <johnny.c.rich@gmail.com>
2025-07-23 16:36:11 -0400

    refactor: layout changes on revamped data tab to be responsive.

--- client/lib/features/admin/presentation/views/events_tab.dart
+++ client/lib/features/admin/presentation/views/events_tab.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:client/core/localization/localization_helper.dart';
 import 'package:client/core/utils/error_utils.dart';
 import 'package:client/core/widgets/proxied_image.dart';
@@ -9,7 +11,6 @@ import 'package:intl/intl.dart';
 import 'package:client/features/community/data/providers/community_provider.dart';
 import 'package:client/core/widgets/buttons/action_button.dart';
 import 'package:client/core/widgets/empty_page_content.dart';
-import 'package:client/core/widgets/custom_list_view.dart';
 import 'package:client/core/widgets/custom_stream_builder.dart';
 import 'package:client/config/environment.dart';
 
@@ -43,14 +44,6 @@ class _EventsTabState extends State<EventsTab> {
   void dispose() {
     _allEvents.dispose();
     super.dispose();
-  }
-
-  Widget _buildRowEntry({double? width, required Widget child}) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 3),
-      width: width,
-      child: child,
-    );
   }
 
   Widget _buildRecordingSection(Event event) {
@@ -101,130 +94,144 @@ class _EventsTabState extends State<EventsTab> {
   Widget _buildEventRow({
     required int index,
     required Event event,
+    required bool isMobile,
   }) {
     final timeFormat = DateFormat('MMM d yyyy, h:mma');
     final timezone = getTimezoneAbbreviation(event.scheduledTime!);
     final time = timeFormat.format(event.scheduledTime ?? clockService.now());
+
+    // Determine to show participants or those registered based on the event time
     final participantsLabel = '${EventProvider.fromEvent(
       event,
       communityProvider: CommunityProvider.read(context),
-    ).eventParticipants.length} ${event.scheduledTime!.isBefore(DateTime.now()) ? context.l10n.participants.toLowerCase() : context.l10n.registered.toLowerCase()}';
+    ).eventParticipants.length} ${event.scheduledTime!.isBefore(DateTime.now()) ? context.l10n.participants : context.l10n.registered}';
+
+    // This widget builds a row for each event, displaying its details;
+    // it is used in differerent parts of the layout depending on the device type.
+    // It includes the event's participants, public/private status, and type.
+    final detailsWidget = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Icon(
+              Icons.group_outlined,
+            ),
+            Text(
+              participantsLabel,
+              style: context.theme.textTheme.labelLarge,
+            ),
+          ],
+        ),
+        // SizedBox(width: 48),
+        Row(
+          children: [
+            Icon(
+              event.isPublic == true
+                  ? Icons.language_outlined
+                  : Icons.lock_outline,
+            ),
+            Text(
+              event.isPublic == true ? 'Public' : 'Private',
+              style: context.theme.textTheme.labelLarge,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              event.isLiveStream
+                  ? Icons.live_tv_outlined
+                  : event.isHosted
+                      ? Icons.chair_outlined
+                      : Icons.deck_outlined,
+            ),
+            SizedBox(width: 4),
+            Text(
+              event.isLiveStream
+                  ? 'Livestream'
+                  : event.isHosted
+                      ? 'Hosted'
+                      : 'Hostless',
+              style: context.theme.textTheme.labelLarge,
+            ),
+          ],
+        ),
+      ],
+    );
 
     return Column(
       children: [
         SizedBox(
           height: 10,
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,crossAxisAlignment: CrossAxisAlignment.start,
+        Flex(
+          direction: isMobile ? Axis.vertical : Axis.horizontal,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-        Row(
-          // mainAxisSize: MainAxisSize.max,
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ProxiedImage(
-              event.image,
-              width: 80,
-              height: 80,
-            ),
-            SizedBox(width: 8),
-            Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Text(
-                    event.title ?? 'NO TITLE',
-                    style: context.theme.textTheme.titleLarge,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    '$time $timezone',
-                    style: context.theme.textTheme.bodyMedium,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  ConstrainedBox(
-                    constraints: BoxConstraints(maxHeight: 100, maxWidth: 800),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.group_outlined,
-                                  ),
-                                  Text(
-                                    participantsLabel,
-                                    style: context.theme.textTheme.labelLarge,
-                                  ),
-                                ],
-                              ),
-                              // SizedBox(width: 48),
-                              Row(
-                                children: [
-                                  Icon(
-                                    event.isPublic == true
-                                        ? Icons.language_outlined
-                                        : Icons.lock_outline,
-                                  ),
-                                  Text(
-                                    event.isPublic == true
-                                        ? 'Public'
-                                        : 'Private',
-                                    style: context.theme.textTheme.labelLarge,
-                                  ),
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Icon(
-                                    event.isLiveStream
-                                        ? Icons.live_tv_outlined
-                                        : event.isHosted
-                                            ? Icons.waving_hand_outlined
-                                            : Icons.chair_outlined,
-                                  ),
-                                  Text(
-                                    event.isLiveStream
-                                        ? 'Live'
-                                        : event.isHosted
-                                            ? 'Hosted'
-                                            : 'Hostless',
-                                    style: context.theme.textTheme.labelLarge,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: SizedBox.shrink(),
-                        ),
-                      ],
+            Row(
+              children: [
+                ProxiedImage(
+                  event.image,
+                  width: 80,
+                  height: 80,
+                ),
+                SizedBox(width: 8),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text(
+                      event.title ?? 'NO TITLE',
+                      style: context.theme.textTheme.titleLarge,
                     ),
-                  ),
-              
-                ],
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      '$time $timezone',
+                      style: context.theme.textTheme.bodyMedium,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    if (!isMobile) ...[
+                      ConstrainedBox(
+                        constraints:
+                            BoxConstraints(maxHeight: 100, maxWidth: 400),
+                        child: Expanded(child: detailsWidget),
+                      ),
+                    ],
+                  ],
+                ),
+              ],
+            ),
+            if (isMobile) ...[
+              SizedBox(
+                height: 10,
               ),
-     
+              detailsWidget,
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: _buildRecordingSection(event),
+              ),
+            ] else ...[
+              _buildRecordingSection(event),
+            ],
           ],
         ),
-      _buildRecordingSection(event),
-        ],
-      ),
-      Divider(),
-    ],);
+        Divider(),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    bool showDetails = responsiveLayoutService.isMobile(context);
+    bool isMobile = responsiveLayoutService.isMobile(context);
     return CustomStreamBuilder<List<Event>>(
       stream: _allEvents.stream,
       entryFrom: '_EventsTabState.build',
@@ -265,6 +272,7 @@ class _EventsTabState extends State<EventsTab> {
                           _buildEventRow(
                             index: i,
                             event: events[i],
+                            isMobile: isMobile,
                           ),
                       ],
                     ),
