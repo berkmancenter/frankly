9f2d1fd4a611a6b704eedf71b84e335ccb41ca24
katherineqian <21981137+katherineqian@users.noreply.github.com>
2025-08-07 14:45:08 -0500

    improvement: allow joining meetings in debug mode for dev purposes.

--- client/lib/features/events/features/event_page/presentation/widgets/event_info.dart
+++ client/lib/features/events/features/event_page/presentation/widgets/event_info.dart
@@ -2,6 +2,7 @@ import 'package:client/core/utils/date_utils.dart';
 import 'package:client/core/utils/navigation_utils.dart';
 import 'package:client/core/utils/toast_utils.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:client/features/community/data/providers/community_permissions_provider.dart';
@@ -409,7 +410,7 @@ class _EventInfoState extends State<EventInfo> {
       expand: true,
       onPressed: () async {
         final successfullyJoined =
-            await widget.onJoinEvent(enterMeeting: isEventOpen);
+            await widget.onJoinEvent(enterMeeting: isEventOpen || kDebugMode);
         if (!isEventOpen && !successfullyJoined) {
           // If the event is not open yet, we expect user not to be able to join.
           await showAlert(
