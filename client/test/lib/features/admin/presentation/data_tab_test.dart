fd3565cdab2b34bacd7e56ccf32428ac52597e81
Johnny Richardson <johnny.c.rich@gmail.com>
2025-12-17 13:20:37 -0500

    test(client): reenable data tab test.

--- client/test/lib/features/admin/presentation/data_tab_test.dart
+++ client/test/lib/features/admin/presentation/data_tab_test.dart
@@ -8,7 +8,6 @@ import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 import 'package:client/features/admin/presentation/views/data_tab.dart';
 import 'package:client/features/community/data/providers/community_provider.dart';
-// import 'package:client/core/utils/firestore_utils.dart';
 import 'package:data_models/events/event.dart';
 
 import '../../../../mocked_classes.mocks.dart';
@@ -61,68 +60,68 @@ void main() {
   }
 
   group('DataTab', () {
-    // testWidgets('should display empty page when no events',
-    //     (WidgetTester tester) async {
-    //   mockAllEvents = [];
+    testWidgets('should display empty page when no events',
+        (WidgetTester tester) async {
+      mockAllEvents = [];
 
-    //   await tester.pumpWidget(createWidgetUnderTest());
-    //   await tester.pump();
+      await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pump();
 
-    //   expect(find.byType(EmptyPageContent), findsOneWidget);
-    // });
+      expect(find.byType(EmptyPageContent), findsOneWidget);
+    });
 
-    // testWidgets('should display events list when events exist', (WidgetTester tester) async {
-    //   when(mockAllEvents).thenAnswer((_) => [mockAllEvents[0]]);
+    testWidgets('should display events list when events exist', (WidgetTester tester) async {
+      when(mockAllEvents).thenAnswer((_) => [mockAllEvents[0]]);
 
-    //   await tester.pumpWidget(createWidgetUnderTest());
-    //   await tester.pump();
+      await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pump();
 
-    //   expect(find.text('Test Event 1'), findsOneWidget);
-    //   expect(find.text('Public'), findsOneWidget);
-    //   expect(find.text('Hosted'), findsOneWidget);
-    // });
+      expect(find.text('Test Event 1'), findsOneWidget);
+      expect(find.text('Public'), findsOneWidget);
+      expect(find.text('Hosted'), findsOneWidget);
+    });
 
-    // testWidgets('should display pagination controls', (WidgetTester tester) async {
-    //   when(mockAllEvents).thenAnswer((_) => mockAllEvents);
+    testWidgets('should display pagination controls', (WidgetTester tester) async {
+      when(mockAllEvents).thenAnswer((_) => mockAllEvents);
 
-    //   await tester.pumpWidget(createWidgetUnderTest());
-    //   await tester.pump();
+      await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pump();
 
-    //   expect(find.byIcon(Icons.arrow_back_rounded), findsOneWidget);
-    //   expect(find.byIcon(Icons.arrow_forward_rounded), findsOneWidget);
-    //   expect(find.text('1 - 5 of 80'), findsOneWidget);
-    // });
+      expect(find.byIcon(Icons.arrow_back_rounded), findsOneWidget);
+      expect(find.byIcon(Icons.arrow_forward_rounded), findsOneWidget);
+      expect(find.text('1 - 5 of 80'), findsOneWidget);
+    });
 
-    // testWidgets('should navigate to next page when forward button pressed', (WidgetTester tester) async {
-    //   when(mockAllEvents).thenAnswer((_) => mockAllEvents);
+    testWidgets('should navigate to next page when forward button pressed', (WidgetTester tester) async {
+      when(mockAllEvents).thenAnswer((_) => mockAllEvents);
 
-    //   await tester.pumpWidget(createWidgetUnderTest());
-    //   await tester.pump();
+      await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pump();
 
-    //   await tester.tap(find.byIcon(Icons.arrow_forward_rounded));
-    //   await tester.pump();
+      await tester.tap(find.byIcon(Icons.arrow_forward_rounded));
+      await tester.pump();
 
-    //   expect(find.text('6 - 10 of 80'), findsOneWidget);
-    // });
-    // testWidgets('should display correct livestream status icons', (WidgetTester tester) async {
-    //   when(mockAllEvents.where((event) => event.nullableEventType == EventType.livestream))
-    //       .thenReturn([mockAllEvents[0]]);
+      expect(find.text('6 - 10 of 80'), findsOneWidget);
+    });
+    testWidgets('should display correct livestream status icons', (WidgetTester tester) async {
+      when(mockAllEvents.where((event) => event.nullableEventType == EventType.livestream))
+          .thenReturn([mockAllEvents[0]]);
 
-    //   await tester.pumpWidget(createWidgetUnderTest());
-    //   await tester.pump();
+      await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pump();
 
-    //   expect(find.byIcon(Icons.live_tv_outlined), findsOneWidget);
-    //   expect(find.text('Livestream'), findsOneWidget);
-    // });
+      expect(find.byIcon(Icons.live_tv_outlined), findsOneWidget);
+      expect(find.text('Livestream'), findsOneWidget);
+    });
 
-    // testWidgets('should show private event correctly', (WidgetTester tester) async {
-    //   when(mockAllEvents.where((event) => !event.isPublic)).thenReturn([mockAllEvents[0]]);
+    testWidgets('should show private event correctly', (WidgetTester tester) async {
+      when(mockAllEvents.where((event) => !event.isPublic)).thenReturn([mockAllEvents[0]]);
 
-    //   await tester.pumpWidget(createWidgetUnderTest());
-    //   await tester.pump();
+      await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pump();
 
-    //   expect(find.byIcon(Icons.lock_outline), findsOneWidget);
-    //   expect(find.text('Private'), findsOneWidget);
-    // });
+      expect(find.byIcon(Icons.lock_outline), findsOneWidget);
+      expect(find.text('Private'), findsOneWidget);
+    });
   });
 }
