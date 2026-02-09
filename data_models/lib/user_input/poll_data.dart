f72007cb196facc3dd78b9148312f4047463ab12
katherineqian <21981137+katherineqian@users.noreply.github.com>
2026-02-09 11:22:54 -0600

    feat: add PollData data model which represents one user's response to a poll.

---
+++ data_models/lib/user_input/poll_data.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'poll_data.freezed.dart';
+part 'poll_data.g.dart';
+
+@Freezed(makeCollectionsUnmodifiable: false)
+class PollData with _$PollData {
+  factory PollData({
+    String? id,
+    String? userId,
+    String? userEmail,
+    String? userName,
+    DateTime? answeredDate,
+    String? pollResponse,
+    String? roomId,
+    String? agendaItemId,
+    String? agendaItemTitle,
+    String? pollQuestion,
+  }) = _PollData;
+
+  factory PollData.fromJson(Map<String, dynamic> json) =>
+      _$PollDataFromJson(json);
+}
