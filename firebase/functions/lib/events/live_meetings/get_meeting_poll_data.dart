9da355a0581f7681d5df6a7f273e5d217e3c9fe1
katherineqian <21981137+katherineqian@users.noreply.github.com>
2026-02-09 11:46:12 -0600

    feat: Implement _getPollsFromPath method to return poll data from agenda item in GetMeetingPollData cloud function.

--- firebase/functions/lib/events/live_meetings/get_meeting_poll_data.dart
+++ firebase/functions/lib/events/live_meetings/get_meeting_poll_data.dart
@@ -3,14 +3,11 @@ import 'dart:async';
 import 'package:data_models/user_input/poll_data.dart';
 import 'package:firebase_functions_interop/firebase_functions_interop.dart'
     as functions_interop;
-import 'package:firebase_admin_interop/firebase_admin_interop.dart';
 import '../../on_call_function.dart';
 import '../../utils/infra/firebase_auth_utils.dart';
 import '../../utils/infra/firestore_utils.dart';
 import '../../utils/utils.dart';
 import 'package:data_models/cloud_functions/requests.dart';
-import 'package:data_models/user_input/chat.dart';
-import 'package:data_models/user_input/chat_suggestion_data.dart';
 import 'package:data_models/events/event.dart';
 import 'package:data_models/events/live_meetings/meeting_guide.dart';
 import 'package:data_models/community/membership.dart';
@@ -81,64 +78,67 @@ class GetMeetingPollData extends OnCallMethod<GetMeetingPollDataRequest> {
       ...breakoutMeetingLinks,
     ];
 
-    // final suggestionDataListResults =
-    //     await Future.wait(<Future<List<ChatSuggestionData>>>[
-    //   for (final path in meetingPaths) _getSuggestionsFromPath(path),
-    // ]);
-
-    // final agendaItemSuggestions = await _getAgendaItemSuggestions(event, [
-    //   liveMeetingPath,
-    //   ...breakoutMeetingLinks,
-    // ]);
+    final pollDataListResults = await Future.wait(<Future<List<PollData>>>[
+      for (final path in meetingPaths) _getPollsFromPath(path, event),
+    ]);
 
     return GetMeetingPollDataResponse(
-      polls: [],
+      polls: pollDataListResults.expand((p) => p).toList(),
     ).toJson();
   }
 
-  Future<List<PollData>> _getPollsFromPath(String path) async {
+  Future<List<PollData>> _getPollsFromPath(String path, Event event) async {
     final roomId = path.split('/').last;
-    return [];
-    // final userSuggestionsData = await firestore
-    //     .collection('$path/user-suggestions')
-    //     .orderBy(SuggestedAgendaItem.kFieldCreatedDate)
-    //     .get();
 
-    // final chatSuggestionDataList = <ChatSuggestionData>[];
-    // for (var document in userSuggestionsData.documents) {
-    //   var doc = SuggestedAgendaItem.fromJson(
-    //     firestoreUtils.fromFirestoreJson(document.data.toMap()),
-    //   );
-    //   final memberInfo = await firebaseAuthUtils.getUser(doc.creatorId!);
+    final pollAgendaItems =
+        event.agendaItems.where((a) => a.type == AgendaItemType.poll).toList();
 
-    //   String memberName;
-    //   if (isNullOrEmpty(memberInfo.displayName)) {
-    //     final memberDoc =
-    //         await firestore.document('publicUser/${memberInfo.uid}').get();
-    //     final publicUserInfo = PublicUserInfo.fromJson(
-    //       firestoreUtils.fromFirestoreJson(memberDoc.data.toMap()),
-    //     );
-    //     memberName = publicUserInfo.displayName ?? '';
-    //   } else {
-    //     memberName = memberInfo.displayName;
-    //   }
+    final pollDataList = <PollData>[];
 
-    //   chatSuggestionDataList.add(
-    //     ChatSuggestionData(
-    //       id: document.documentID,
-    //       creatorId: doc.creatorId,
-    //       creatorEmail: memberInfo.email,
-    //       creatorName: memberName,
-    //       createdDate: doc.createdDate,
-    //       message: doc.content,
-    //       type: ChatSuggestionType.suggestion,
-    //       upvotes: doc.upvotedUserIds.length,
-    //       downvotes: doc.downvotedUserIds.length,
-    //       roomId: roomId,
-    //     ),
-    //   );
-    // }
+    for (final agendaItem in pollAgendaItems) {
+      final participantDetailsPath =
+          '$path/participant-agenda-item-details/${agendaItem.id}/participant-details';
 
-    // return chatSuggestionDataList;
+      final documents =
+          await firestore.collection(participantDetailsPath).get();
+
+      for (var document in documents.documents) {
+        final details = ParticipantAgendaItemDetails.fromJson(
+          firestoreUtils.fromFirestoreJson(document.data.toMap()),
+        );
+
+        if (details.pollResponse != null) {
+          final memberInfo = await firebaseAuthUtils.getUser(details.userId!);
+
+          String memberName;
+          if (isNullOrEmpty(memberInfo.displayName)) {
+            final memberDoc =
+                await firestore.document('publicUser/${memberInfo.uid}').get();
+            final publicUserInfo = PublicUserInfo.fromJson(
+              firestoreUtils.fromFirestoreJson(memberDoc.data.toMap()),
+            );
+            memberName = publicUserInfo.displayName ?? '';
+          } else {
+            memberName = memberInfo.displayName;
+          }
+
+          pollDataList.add(
+            PollData(
+              userId: details.userId,
+              userName: memberName,
+              userEmail: memberInfo.email,
+              agendaItemId: agendaItem.id,
+              agendaItemTitle: agendaItem.title,
+              pollQuestion: agendaItem.title,
+              pollResponse: details.pollResponse,
+              roomId: roomId,
+              answeredDate: document.updateTime?.toDateTime(),
+            ),
+          );
+        }
+      }
+    }
+
+    return pollDataList;
   }
 }
