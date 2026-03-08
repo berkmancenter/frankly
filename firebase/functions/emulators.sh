b489736dd9e3676f399cf0f767e568c0e3e0e7bd
mikewillems <14206684+mikewillems@users.noreply.github.com>
2026-03-08 12:07:33 -0700

    improved local emulated start/stop behavior to be less error prone

--- firebase/functions/emulators.sh
+++ firebase/functions/emulators.sh
@@ -1,2 +1,5 @@
-dart run build_runner build --output=build 
-firebase emulators:start --only firestore,functions,auth,pubsub,database --project dev --inspect-functions
+#!/usr/bin/env bash
+set -euo pipefail
+
+dart run build_runner build --output=build
+exec firebase emulators:start --only firestore,functions,auth,pubsub,database --project dev --inspect-functions
