e1336e2fe0e4042b5d866ceb5b3c884106b8c020
Johnny Richardson <johnny.c.rich@gmail.com>
2026-04-28 17:59:07 -0400

    feat(client): toggle for if signup, login buttons should show on main nav.

--- client/lib/core/widgets/navbar/nav_bar/nav_bar.dart
+++ client/lib/core/widgets/navbar/nav_bar/nav_bar.dart
@@ -201,6 +201,8 @@ class NavBarState extends State<NavBar> implements NavBarView {
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: ProfileOrLogin(
             showMenuAboveIcon: false,
+            showLoginSignUp:
+                routerDelegate.currentConfiguration?.uri.path != '/',
           ),
         )
       else ...[
