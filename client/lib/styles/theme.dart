c4682c8aad7cdbb1f65c9ffa164b5989e358339b
katherineqian <21981137+katherineqian@users.noreply.github.com>
2026-06-18 16:22:52 -0500

    lint: rm unused imports in theme.dart

--- client/lib/styles/theme.dart
+++ client/lib/styles/theme.dart
@@ -1,5 +1,3 @@
-import 'package:client/core/utils/transitions.dart';
-import 'package:client/styles/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
