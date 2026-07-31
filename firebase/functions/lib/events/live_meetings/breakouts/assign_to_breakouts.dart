46c76df037a6e72fab230ee67fd4f3f2e20e41c1
katherineqian <21981137+katherineqian@users.noreply.github.com>
2026-07-30 23:10:18 -0500

    feat: call frankly match API to create smart matches if useHostedApi is true.

--- firebase/functions/lib/events/live_meetings/breakouts/assign_to_breakouts.dart
+++ firebase/functions/lib/events/live_meetings/breakouts/assign_to_breakouts.dart
@@ -239,6 +239,12 @@ class AssignToBreakouts {
     // Smart match users who had valid survey responses
     profile('smart matching');
     List<frankly_match.MatchGroup> smartMatches;
+    if (useHostedApi) {
+      smartMatches = await createFranklyMatchApiGroups(
+        participantSurveyResponsesLookup: participantSurveyResponsesLookup,
+        targetParticipantsPerRoom: targetParticipantsPerRoom,
+      );
+    }
     if (targetParticipantsPerRoom <= 2 ||
         participantSurveyResponsesLookup.length <= 2) {
       smartMatches =
