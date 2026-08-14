b7f7f66f73ebfd890f7eee878288a731dacf44d1
Johnny Richardson <johnny.c.rich@gmail.com>
2026-08-14 17:48:58 -0400

    feat(client): update agenda item retrieval to use resolved agenda items.

--- client/lib/features/events/features/live_meeting/features/meeting_guide/data/providers/meeting_guide_card_store.dart
+++ client/lib/features/events/features/live_meeting/features/meeting_guide/data/providers/meeting_guide_card_store.dart
@@ -70,7 +70,7 @@ class MeetingGuideCardStore with ChangeNotifier {
     }
 
     return meetingGuideCardAgendaItem?.id ??
-        agendaProvider.agendaItems.firstOrNull?.id;
+        agendaProvider.resolvedAgendaItems.firstOrNull?.id;
   }
 
   String? get _agendaProviderCurrentItemId {
@@ -81,12 +81,13 @@ class MeetingGuideCardStore with ChangeNotifier {
     }
 
     return agendaProvider.currentAgendaItem?.id ??
-        agendaProvider.agendaItems.firstOrNull?.id;
+        agendaProvider.resolvedAgendaItems.firstOrNull?.id;
   }
 
-  AgendaItem? get meetingGuideCardAgendaItem => agendaProvider.agendaItems
-      .where((i) => i.id == _currentMeetingGuideAgendaItemId)
-      .firstOrNull;
+  AgendaItem? get meetingGuideCardAgendaItem =>
+      agendaProvider.resolvedAgendaItems
+          .where((i) => i.id == _currentMeetingGuideAgendaItemId)
+          .firstOrNull;
 
   bool get meetingGuideCardIsPending =>
       _pendingMeetingGuideAgendaItemTimer?.isActive ?? false;
@@ -244,17 +245,18 @@ class MeetingGuideCardStore with ChangeNotifier {
 
   Future<void> goToPreviousAgendaItem() async {
     final currentAgendaItemId = meetingGuideCardAgendaItem?.id;
-    final currentAgendaItemIndex = agendaProvider.agendaItems
+    final currentAgendaItemIndex = agendaProvider.resolvedAgendaItems
         .indexWhere((a) => a.id == currentAgendaItemId);
 
     final AgendaItem prevAgendaItem;
     if (currentAgendaItemIndex < 0 && agendaProvider.isMeetingFinished) {
-      prevAgendaItem = agendaProvider.agendaItems.last;
+      prevAgendaItem = agendaProvider.resolvedAgendaItems.last;
     } else if (currentAgendaItemIndex < 0) {
       throw VisibleException('Meeting Guide entry not found.');
     } else {
-      prevAgendaItem =
-          agendaProvider.agendaItems.skip(currentAgendaItemIndex - 1).first;
+      prevAgendaItem = agendaProvider.resolvedAgendaItems
+          .skip(currentAgendaItemIndex - 1)
+          .first;
     }
 
     await agendaProvider.goToPreviousAgendaItem(prevAgendaItem.id);
